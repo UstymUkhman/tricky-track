b7c4eef32cde40898ae2423a619d56fc0fcf2e99
Ustym <ustym.ukhman@tbwa-x.com>
2024-02-29 19:25:40 +0200

    Update Physics Manager

---
+++ src/physics/constants.js
@@ -0,0 +1,76 @@
+// Activation state constants:
+export const ACTIVE_TAG           = 1;
+export const ISLAND_SLEEPING      = 2;
+export const WANTS_DEACTIVATION   = 3;
+export const DISABLE_DEACTIVATION = 4;
+export const DISABLE_SIMULATION   = 5;
+
+// Rigid Body constants:
+export const RIGID_MARGIN          = 0.01;
+export const RIGID_FRICTION        = 0.5;
+export const RIGID_RESTITUTION     = 0.0;
+export const RIGID_LINEAR_DAMPING  = 0.0;
+export const RIGID_ANGULAR_DAMPING = 0.0;
+
+// Soft Body constants:
+// export const SOFT_FRICTION    = 0.5;
+// export const SOFT_MARGIN      = 0.02;
+// export const SOFT_DAMPING     = 0.01;
+// export const SOFT_STIFFNESS   = 1.0;
+// export const SOFT_PITERATIONS = 40;
+// export const SOFT_VITERATIONS = 40;
+// export const SOFT_RESTITUTION = 0.0;
+
+// Cloth Body constants:
+// export const CLOTH_MARGIN      = 0.03;
+// export const CLOTH_PITERATIONS = 10;
+// export const CLOTH_VITERATIONS = 10;
+
+// Rope Body constants:
+// export const ROPE_MARGIN      = 0.05;
+// export const ROPE_PITERATIONS = 10;
+// export const ROPE_VITERATIONS = 10;
+
+// Constraint constants:
+// export const GENERIC_VELOCITY     =  100;
+// export const GENERIC_MAX_FORCE    = 1000;
+// export const CONETWIST_IMPULSE    =  100;
+// export const HINGE_ACCELERATION   =  100;
+// export const SLIDER_ACCELERATION  =   50;
+// export const CONSTRAINT_THRESHOLD =    0;
+
+// Collision constants:
+// export const STATIC_COLLISION     =    1;
+// export const KINEMATIC_COLLISION  =    2;
+// export const IGNORED_COLLISION    =    4;
+// export const SOFT_COLLISION       = 0x11;
+// export const CCD_MOTION_THRESHOLD = 1e-5;
+
+// Group constants:
+// export const GROUP_NONE      =    0;
+// export const GROUP_DEFAULT   =    1;
+export const GROUP_STATIC    =    2;
+// export const GROUP_KINEMATIC =    4;
+export const GROUP_DYNAMIC   =  128;
+// export const GROUP_SOFT      =  256;
+// export const GROUP_CLOTH     =  512;
+// export const GROUP_ROPE      = 1024;
+
+// Mask constants
+export const MASK_ALL                     = 0xFFFF;
+// export const MASK_SOFT                    = GROUP_SOFT;
+// export const MASK_ROPE                    = GROUP_ROPE;
+// export const MASK_CLOTH                   = GROUP_CLOTH;
+// export const MASK_STATIC                  = GROUP_STATIC;
+// export const MASK_DYNAMIC                 = GROUP_DYNAMIC;
+// export const MASK_KINEMATIC               = GROUP_KINEMATIC;
+// export const MASK_STATIC_AND_KINEMATIC    = GROUP_STATIC  | GROUP_KINEMATIC;
+// export const MASK_FLEX                    = GROUP_SOFT    | GROUP_CLOTH     | GROUP_ROPE;
+// export const MASK_RIGID                   = GROUP_STATIC  | GROUP_KINEMATIC | GROUP_DYNAMIC;
+
+// export const MASK_NOT_SOFT                = 0xFFFF ^ GROUP_SOFT;
+// export const MASK_NOT_CLOTH               = 0xFFFF ^ GROUP_CLOTH;
+// export const MASK_NOT_STATIC              = 0xFFFF ^ GROUP_STATIC;
+// export const MASK_NOT_DYNAMIC             = 0xFFFF ^ GROUP_DYNAMIC;
+// export const MASK_NOT_KINEMATIC           = 0xFFFF ^ GROUP_KINEMATIC;
+// export const MASK_NOT_STATIC_OR_KINEMATIC = 0xFFFF ^ MASK_STATIC_AND_KINEMATIC;
--- src/physics/index.js
+++ src/physics/index.js
@@ -1,8 +1,139 @@
+/**
+ * @typedef {object} DynamicBody
+ * @property {import("three").Mesh} mesh
+ * @property {any} body
+ */
+
 const Ammo = await (await import("./Ammo")).default();
+import { Vector3 } from "three/src/math/Vector3";
+import { PI } from "../utils/Number";
+
+import {
+    MASK_ALL,
+    GROUP_STATIC,
+    RIGID_MARGIN,
+    GROUP_DYNAMIC,
+    RIGID_FRICTION,
+    RIGID_RESTITUTION,
+    RIGID_LINEAR_DAMPING,
+    RIGID_ANGULAR_DAMPING
+} from "./constants";
 
 class Physics
 {
-    #engine = Ammo;
+    #Engine = Ammo;
+
+    #friction = RIGID_FRICTION;
+    #restitution = RIGID_RESTITUTION;
+    #linearDamping = RIGID_LINEAR_DAMPING;
+    #angularDamping = RIGID_ANGULAR_DAMPING;
+
+    #linearFactor = new Vector3(1.0, 1.0, 1.0);
+    #angularFactor = new Vector3(1.0, 1.0, 1.0);
+    #transform = new this.#Engine.btTransform();
+
+    /** @type {DynamicBody[]} */ #dynamicBodies = [];
+    #broadphase = new this.#Engine.btDbvtBroadphase();
+
+    #collision = new this.#Engine.btDefaultCollisionConfiguration();
+    #solver = new this.#Engine.btSequentialImpulseConstraintSolver();
+    #dispatcher = new this.#Engine.btCollisionDispatcher(this.#collision);
+
+    #world = new this.#Engine.btDiscreteDynamicsWorld(
+        this.#dispatcher, this.#broadphase, this.#solver, this.#collision
+    );
+
+    constructor()
+    {
+        this.#world.setGravity(new this.#Engine.btVector3(0, -9.81, 0));
+    }
+
+    /**
+     * @param {unknown} shape
+     * @param {import("three").Vector3} position
+     * @param {import("three").Quaternion} quaternion
+     * @param {number} mass
+     * @param {number} margin
+     * @returns {any} body
+     */
+    #createRigidBody(shape, position, quaternion, mass = 0, margin = RIGID_MARGIN)
+    {
+        const transform = new this.#Engine.btTransform();
+        const inertia = new this.#Engine.btVector3(0, 0, 0);
+
+        mass && shape.calculateLocalInertia(mass, inertia);
+        margin !== RIGID_MARGIN && shape.setMargin(margin);
+
+        transform.setIdentity();
+        transform.setOrigin(new this.#Engine.btVector3(...position));
+        transform.setRotation(new this.#Engine.btQuaternion(...quaternion));
+
+        const body = new this.#Engine.btRigidBody(
+            new this.#Engine.btRigidBodyConstructionInfo(
+                mass, new this.#Engine.btDefaultMotionState(transform), shape, inertia
+            )
+        );
+
+        body.setAngularFactor(new this.#Engine.btVector3(...this.#angularFactor));
+        body.setLinearFactor(new this.#Engine.btVector3(...this.#linearFactor));
+        body.setDamping(this.#linearDamping, this.#angularDamping);
+        body.setRestitution(this.#restitution);
+        body.setFriction(this.#friction);
+
+        return body;
+    }
+
+    /** @param {import("three").Mesh} mesh @param {unknown} shape @param {number} mass */
+    #addDynamicBody(mesh, shape, mass = 0)
+    {
+        const body = this.#createRigidBody(shape, mesh.position.clone(), mesh.quaternion.clone(), mass);
+        this.#world.addRigidBody(body, GROUP_DYNAMIC, MASK_ALL);
+        this.#dynamicBodies.push({ mesh, body });
+    }
+
+    /** @param {import("three").Mesh} mesh @param {unknown} shape */
+    #addStaticBody(mesh, shape)
+    {
+        this.#world.addRigidBody(this.#createRigidBody(
+            shape, mesh.position.clone(), mesh.quaternion.clone()
+        ), GROUP_STATIC, MASK_ALL);
+    }
+
+    /** @param {import("three").Mesh} mesh @param {number} mass */
+    addDynamicBox(mesh, mass = 0)
+    {
+        const { width, height, depth } = mesh.geometry.parameters;
+
+        this.#addDynamicBody(mesh, new this.#Engine.btBoxShape(
+            new this.#Engine.btVector3(width * 0.5, height * 0.5, depth * 0.5)
+        ), mass);
+    }
+
+    /** @param {import("three").Mesh} mesh */
+    addStaticPlane(mesh)
+    {
+        this.#addStaticBody(mesh, new this.#Engine.btStaticPlaneShape(
+            new this.#Engine.btVector3(0, 0, mesh.rotation.x / -PI.m2), 0
+        ));
+    }
+
+    /** @param {number} delta */
+    update(delta)
+    {
+        for (let b = 0, l = this.#dynamicBodies.length; b < l; b++)
+        {
+            const { body, mesh } = this.#dynamicBodies[b];
+            body.getMotionState().getWorldTransform(this.#transform);
+
+            const origin = this.#transform.getOrigin();
+            const rotation = this.#transform.getRotation();
+
+            mesh.position.set(origin.x(), origin.y(), origin.z());
+            mesh.quaternion.set(rotation.x(), rotation.y(), rotation.z(), rotation.w());
+        }
+
+        this.#world.stepSimulation(delta, 10);
+    }
 }
 
 export default new Physics();
